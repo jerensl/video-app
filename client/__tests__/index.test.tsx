3ec8c5ff5d1538a46ff420a9f61d5468cb7886e4
jerensl <jerenslensun@gmail.com>
2021-10-29 08:23:51 +0800

    create test case for input room id

--- client/__tests__/index.test.tsx
+++ client/__tests__/index.test.tsx
@@ -2,18 +2,34 @@
  * @jest-environment jsdom
  */
 
- import React from 'react'
- import { render, screen } from '@testing-library/react'
- import Home from '../pages/index'
- 
- describe('Home', () => {
-   it('renders a heading', () => {
-     render(<Home />)
- 
-     const heading = screen.getByRole('heading', {
-       name: /Join A Meeting/i,
-     })
- 
-     expect(heading).toBeInTheDocument()
-   })
- })
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Home from '../pages/index'
+
+const renderComponent = () => {
+    const utils = render(<Home />)
+
+    const input = utils.getByLabelText('room-input') as HTMLInputElement
+
+    return {
+        input,
+        ...utils,
+    }
+}
+
+test('renders a heading', () => {
+    const { getByRole } = renderComponent()
+
+    const heading = getByRole('heading', {
+        name: /Join A Meeting/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+})
+
+test('insert room id', () => {
+    const { input } = renderComponent()
+
+    fireEvent.change(input, { target: { value: '1234' } })
+    expect(input.value).toBe('1234')
+})
